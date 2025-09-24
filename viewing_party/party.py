a87e8141a044e82c83b5e8e1f14c007ec91015d3
Amanda Thompson <thompson.adg@gmail.com>
2025-09-23 22:52:21 -0700

    updated watch_movie function with test cases

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -18,9 +18,13 @@ def add_to_watchlist(user_data, movie):
     return user_data
 
 def watch_movie(user_data, title):
-    for movie in user_data["watchlist"]:
+    movie_to_pop = -1
+    for i in range(len(user_data["watchlist"])):
+        movie = user_data["watchlist"][i]
         if movie["title"] == title:
-            user_data["watched"].append(user_data["watchlist"].pop(movie))
+            movie_to_pop = i
+    if movie_to_pop > -1:  
+        user_data["watched"].append(user_data["watchlist"].pop(movie_to_pop))
     
     return user_data
 
