1de2fa7660b46006232293b1d8efc37f2c2d32f9
Hsiangting <stpeng777@gmail.com>
2025-09-24 12:29:44 -0700

    implemented party.py with test_wave_01

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,23 +1,66 @@
 # ------------- WAVE 1 --------------------
 
-def create_movie(title, genre, rating):
+# def create_movie(title, genre, rating):
+#     if title is None or genre is None or rating is None :
+#         return None
+
+#     new_movie = dict(title = title, genre = genre, rating = rating)
+
+#     return new_movie
+
+# def add_to_watched(user_data, movie):
+#     watched_list = user_data["watched"]
+#     watched_list.append(movie)
+
+#     return user_data
+
+# def add_to_watchlist(user_data, movie):
+#     watchlist = user_data["watchlist"]
+#     watchlist.append(movie)
+
+#     return user_data
+
+# def watch_movie(user_data, movie_title):
+#     movie_title_to_move = None
+
+#     for movie in user_data["watchlist"] :
+#         if movie["title"] == movie_title :
+#             movie_title_to_move = movie
+#             break
+#     if movie_title_to_move is not None:
+#         user_data["watched"].append(movie_title_to_move)
+#         user_data["watchlist"].remove(movie_title_to_move)
+
+#     return user_data
+
+
+
+# ------------- WAVE 2 --------------------
+
+def calculates_watched_average_rating():
     pass
 
-# -----------------------------------------
-# ------------- WAVE 2 --------------------
-# -----------------------------------------
+def empty_watched_average_rating_is_zero():
+    pass
 
+def most_watched_genre():
+    pass
 
-# -----------------------------------------
+def most_watched_genre_order_mixed():
+    pass
+
+def genre_is_None_if_empty_watched():
+    pass
+
 # ------------- WAVE 3 --------------------
-# -----------------------------------------
 
-        
-# -----------------------------------------
+
+
+
 # ------------- WAVE 4 --------------------
-# -----------------------------------------
 
-# -----------------------------------------
+
+
 # ------------- WAVE 5 --------------------
-# -----------------------------------------
+
 
